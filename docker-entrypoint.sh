f73134c338015a3d80c442778a8dd2db83c6324e
Rafael Guerra <rcguerra82@gmail.com>
2017-10-30 13:07:08 -0300

    update taiga conf.json paths

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,29 +30,29 @@ if [ ! -d "/usr/src/taiga-back/static" ]; then
 fi
 
 # Automatically replace "TAIGA_HOSTNAME" with the environment variable
-sed -i "s/TAIGA_HOSTNAME/$TAIGA_HOSTNAME/g" /taiga/conf.json
+sed -i "s/TAIGA_HOSTNAME/$TAIGA_HOSTNAME/g" /conf/taiga/conf.json.json
 
 # Look to see if we should set the "eventsUrl"
 if [ ! -z "$RABBIT_PORT_5672_TCP_ADDR" ]; then
   echo "Enabling Taiga Events"
-  sed -i "s/eventsUrl\": null/eventsUrl\": \"ws:\/\/$TAIGA_HOSTNAME\/events\"/g" /taiga/conf.json
+  sed -i "s/eventsUrl\": null/eventsUrl\": \"ws:\/\/$TAIGA_HOSTNAME\/events\"/g" /conf/taiga/conf.json.json
   mv /etc/nginx/taiga-events.conf /etc/nginx/conf.d/default.conf
 fi
 
 # Handle enabling/disabling SSL
 if [ "$TAIGA_SSL_BY_REVERSE_PROXY" = "True" ]; then
   echo "Enabling external SSL support! SSL handling must be done by a reverse proxy or a similar system"
-  sed -i "s/http:\/\//https:\/\//g" /taiga/conf.json
-  sed -i "s/ws:\/\//wss:\/\//g" /taiga/conf.json
+  sed -i "s/http:\/\//https:\/\//g" /conf/taiga/conf.json.json
+  sed -i "s/ws:\/\//wss:\/\//g" /conf/taiga/conf.json.json
 elif [ "$TAIGA_SSL" = "True" ]; then
   echo "Enabling SSL support!"
-  sed -i "s/http:\/\//https:\/\//g" /taiga/conf.json
-  sed -i "s/ws:\/\//wss:\/\//g" /taiga/conf.json
+  sed -i "s/http:\/\//https:\/\//g" /conf/taiga/conf.json.json
+  sed -i "s/ws:\/\//wss:\/\//g" /conf/taiga/conf.json.json
   mv /etc/nginx/ssl.conf /etc/nginx/conf.d/default.conf
-elif grep -q "wss://" "/taiga/conf.json"; then
+elif grep -q "wss://" "/conf/taiga/conf.json.json"; then
   echo "Disabling SSL support!"
-  sed -i "s/https:\/\//http:\/\//g" /taiga/conf.json
-  sed -i "s/wss:\/\//ws:\/\//g" /taiga/conf.json
+  sed -i "s/https:\/\//http:\/\//g" /conf/taiga/conf.json.json
+  sed -i "s/wss:\/\//ws:\/\//g" /conf/taiga/conf.json.json
 fi
 
 # Start nginx service (need to start it as background process)
